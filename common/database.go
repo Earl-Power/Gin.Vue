ece844fa07a7737abbe2283943cdc54f4c050b81
Earl <linux107@gmail.com>
2023-08-18 01:19:17 +0800

    rebuild database connect from viper

--- common/database.go
+++ common/database.go
@@ -1,7 +1,9 @@
 package common
 
 import (
+	"fmt"
 	"github.com/Earl-Power/Gin.Vue/models"
+	"github.com/spf13/viper"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -10,24 +12,27 @@ var DB *gorm.DB
 
 // InitDB 数据库连接
 func InitDB() *gorm.DB {
-	/*
-		driverName := "mysql"
-		host := "localhost"
-		port := "3306"
-		username := "root"
-		password := "root"
-		database := "gin_vue"
-		charset := "utf8"
-		args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
-			username,
-			password,
-			host,
-			port,
-			database,
-			charset,
-		)
-	*/
-	dsn := "root:root@tcp(localhost:3306)/gin_vue?charset=utf8&parseTime=True"
+	//driverType := viper.GetString("datasource.driverType")
+	host := viper.GetString("datasource.host")
+	dbPort := viper.GetString("datasource.db_port")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	database := viper.GetString("datasource.database")
+	charset := viper.GetString("datasource.charset")
+
+	//args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
+	//	username,
+	//	password,
+	//	host,
+	//	dbPort,
+	//	database,
+	//	charset,
+	//)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True",
+		username, password, host, dbPort, database, charset)
+
+	//dsn := "root:root@tcp(localhost:3306)/gin_vue?charset=utf8&parseTime=True"
+	//db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect databases, err:" + err.Error())
